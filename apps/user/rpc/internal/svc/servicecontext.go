535890a91ffca444b6fd52a0025d8eaed69719f2
yllmis <2943246638@qq.com>
2026-04-09 11:27:31 +0800

    feat(user rpc)：整体jwt鉴权

--- apps/user/rpc/internal/svc/servicecontext.go
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -1,8 +1,13 @@
 package svc
 
 import (
+	"time"
+
 	"github.com/IM_System/apps/user/models"
 	"github.com/IM_System/apps/user/rpc/internal/config"
+	"github.com/IM_System/pkg/constants"
+	"github.com/IM_System/pkg/ctxdata"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
@@ -10,6 +15,7 @@ type ServiceContext struct {
 	Config config.Config
 
 	models.UsersModel
+	Redisx *redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -18,6 +24,19 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
 
+		Redisx: redis.MustNewRedis(c.Redisx),
+
 		UsersModel: models.NewUsersModel(sqlConn, c.Cache),
 	}
+}
+
+func (svc *ServiceContext) SetRootToken() error {
+	// 生成一个jwt，存储在 Redis 中，并设置过期时间长
+	systemToken, err := ctxdata.GetTokenKey(svc.Config.JwtAuth.AccessSecret, time.Now().Unix(), 999999999, constants.SYSTEM_ROOT_ID)
+	if err != nil {
+		return err
+	}
+
+	return svc.Redisx.Set(constants.REDIS_SYSTEM_ROOT_TOKEN, systemToken)
+
 }
